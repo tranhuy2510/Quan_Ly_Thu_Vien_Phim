4ade77ea6df7bde91640377501621d423f4c4522
ADMIN <ADMIN@DESKTOP-UK8EPA0>
2024-12-02 23:25:07 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Quan_Ly_Thu_Vien_Phim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace Quan_Ly_Thu_Vien_Phim
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
